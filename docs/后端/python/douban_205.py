cf90e3adf465487a0b62f247debde0dbdcc646ff
Ricky Chen <809155736@qq.com>
2024-04-24 10:14:26 +0800

    Update douban_205.py

--- docs/后端/python/douban_205.py
+++ docs/后端/python/douban_205.py
@@ -2,9 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import xlwt
 
-
 def request_douban(url):
-headers = {
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/88.0.4324.146 Safari/537.36',
     }
